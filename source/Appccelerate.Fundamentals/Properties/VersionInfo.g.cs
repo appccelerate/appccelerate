cabaa6266231c9792f5251f752d553984d3e0261
Urs Enzler <urs.enzler@gmail.com>
2012-02-12 10:36:42 +0100

    reset version to 0.0.0.0 for dev

--- source/Appccelerate.Fundamentals/Properties/VersionInfo.g.cs
+++ source/Appccelerate.Fundamentals/Properties/VersionInfo.g.cs
@@ -11,6 +11,6 @@ using System.Reflection;
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: AssemblyVersionAttribute("1.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: AssemblyVersionAttribute("0.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("0.0.0.0")]
 
