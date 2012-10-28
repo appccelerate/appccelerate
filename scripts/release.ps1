280f556f7bc770f0549aa2315bb999171f51b934
danielmarbach <daniel.marbach@openplace.net>
2012-10-28 12:13:44 +0100

    Powershell script for auto pushing of nuget packages (assumes that correct packages are in publish folder)

---
+++ scripts/release.ps1
@@ -0,0 +1,14 @@
+$nuget = "..\source\.nuget\nuget.exe"
+$apikey = read-host 'Api Key:'
+& $nuget SetApiKey $apikey 
+$packages = get-childitem ..\publish\Nuget | where {$_.extension -eq ".nupkg" -and !$_.Name.EndsWith("symbols.nupkg")}
+foreach ($package in $packages)
+{
+	write-host $package
+	$answer = read-host 'Upload package (y/n)?'
+	if($answer -eq 'y')
+	{
+		write-host 'Pushing package' $package 
+		& $nuget push $package.FullName
+	}
+}
